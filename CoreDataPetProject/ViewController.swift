59a158c77e4c8bfbb412cebcc317e1ec2d88c3ef
Anton Filippov <derevenchshina@yandex.ru>
2022-08-08 18:20:48 +0500

    app design add data.plist for first db load

--- CoreDataPetProject/ViewController.swift
+++ CoreDataPetProject/ViewController.swift
@@ -6,9 +6,32 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageVIew: UIImageView!
+    @IBOutlet weak var lastStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiseImage: UIImageView!
+    
+    @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
+    }
+    
+    
+    @IBAction func startEngineButtonPressed() {
+    }
+    
+    @IBAction func rateButtonPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
